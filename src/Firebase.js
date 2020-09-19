14ec0a25011b30fc7b788ad3596ecf883b241681
ankit619 <ankitkumar619619@gmail.com>
2020-09-19 10:33:58 +0530

    Cleaning and initial files setup

---
+++ src/Firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCeZblIOz1mAP1lH3_1UDywS3MUahQcOlw",
+    authDomain: "ab-26365.firebaseapp.com",
+    databaseURL: "https://ab-26365.firebaseio.com",
+    projectId: "ab-26365",
+    storageBucket: "ab-26365.appspot.com",
+    messagingSenderId: "213865581104",
+    appId: "1:213865581104:web:5abbd8caed2046c518fbf5",
+    measurementId: "G-FMWDT3CKY5"
+};
