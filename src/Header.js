d5b6cc37169936e8b8dd4e2df9140b8fdbf2758c
ankit619 <ankitkumar619619@gmail.com>
2020-09-19 13:46:21 +0530

    Header created

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import { ShoppingBasket } from '@material-ui/icons';
 
 function Header() {
     return (
-        <h1>Header</h1>
+        <div className="header">
+            {/*Amazon logo*/}
+            <img
+                className="header__logo"
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="Amazon_logo" />
+
+            {/*Search bar*/}
+            <div className="header__search">
+                {/*Search input */}
+                <input
+                    className="header__searchInput"
+                    type="text" />
+                {/*Search logo */}
+                <SearchIcon
+                    className="header__searchIcon" />
+            </div>
+
+            {/*Navigation Icons */}
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLineOne">Hello Guest</span>
+                    <span className="header__optionLineTwo">Sign In</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Returns</span>
+                    <span className="header__optionLineTwo">& Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="header__optionLineOne">Your</span>
+                    <span className="header__optionLineTwo">Prime</span>
+                </div>
+                {/*basket Logo */}
+                <div className="header__optionBasket">
+                    <ShoppingBasket />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
+                </div>
+            </div>
+        </div>
     )
 };
 
